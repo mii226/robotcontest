12d0c67f1a767afe403a03567479ebedffe9928e
mii226 <thai.mii226@gmail.com>
2019-02-14 13:03:57 +0700

    first

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,7 +1,9 @@
 import os
-
+import sys
 from dj_static import Cling #文を新しく記述する
 from django.core.wsgi import get_wsgi_application
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
